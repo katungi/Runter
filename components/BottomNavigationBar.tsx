213870a4b2df424b8e487521a7247153bf4b7ac3
katungi <dankatdennis@gmail.com>
2023-01-17 14:52:50 +0300

    docs: bottom navigation bar documentation

--- components/BottomNavigationBar.tsx
+++ components/BottomNavigationBar.tsx
@@ -2,6 +2,22 @@ import { Box, Center, HStack, Icon, Pressable, Text } from 'native-base';
 import React from 'react';
 import type { BottomNavigationProps } from 'Types/BottomNavigationProps';
 
+/**
+ *
+ * @param navItems - The items of the bottom navigation bar
+ *  `navItems` is an array of objects with the following properties:
+ * - `label`: The label of the bottom navigation bar item
+ * - `icon`: The icon of the bottom navigation bar item
+ * @returns {React.ReactElement} - The bottom navigation bar component
+ *
+ * This is the bottom navigation bar component, it will act as the basis of the bottom part of the scaffold.
+ *  It will contain the items of the bottom navigation bar.
+ * The bottom navigation bar will be a fixed component that will be rendered on the bottom of the screen.
+ *
+ * TODO: Add a prop to change the position of the bottom navigation bar
+ *
+ */
+
 export default function BottomNavigationBar({
   navItems,
 }: BottomNavigationProps) {
