49cd4d2527283b8341ca9b6f0b502c5fb486a4d2
katungi <dankatdennis@gmail.com>
2023-01-17 08:26:25 +0300

    docs: updating the colors docs

--- Defaults/Colors.ts
+++ Defaults/Colors.ts
@@ -1,4 +1,11 @@
 // create a color Object
+/**
+ *
+ * @file Colors.ts
+ * @description This file contains the default colors for the app
+ * This is the default Color object
+ *
+ */
 const Colors = {
   blue: '#2196F3',
   white: '#FFFFFF',
